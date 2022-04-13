8850836d9cf4f5f1d1880363f4105ee5178d9542
vaibrainium <vaibhavt459@gmail.com>
2022-04-12 20:49:15 -0700

    Update setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -2,5 +2,20 @@ from setuptools import find_packages, setup
 
 setup(
     name="{{ cookiecutter.package_name }}",
+    author='{{cookiecutter.full_name}}',
+    license='{{cookiecutter.license}}',
     packages=find_packages(),
+    classifiers=[
+        {% if cookiecutter.license == "MIT" -%}
+        'License :: OSI Approved :: MIT License',
+        {%- elif cookiecutter.license == "BSD-3" -%}
+        'License :: OSI Approved :: BSD License',
+        {%- elif cookiecutter.license == "GNU GPL v3.0" -%}
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        {%- elif cookiecutter.license == "Apache Software License 2.0" -%}
+        'License :: OSI Approved :: Apache Software License',
+        {%- elif cookiecutter.license == "Mozilla Public License 2.0" -%}
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        {%- endif %}
+    ],
 )
