9d009a1e8d42e014815da6bbbe3fc0010633cdd8
vaibrainium <vaibhavt459@gmail.com>
2022-04-12 21:06:40 -0700

    fse

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,12 +11,22 @@ import subprocess
 _logger = logging.getLogger()
 TMP_DIR = './tmp/' 
 
+ALL_TEMP_FOLDERS = ["licenses"]
+
+def remove_temp_folders(temp_folders):
+    for folder in temp_folders:
+        logger.info("Remove temporary folder: %s", folder)
+        shutil.rmtree(folder)
+
 def post_hook():
     """Flag the POST gen HOOK execution"""
     _logger.warning("POST Gen hook executed")
 
     try:
-    	None
+    	for temp_folder in ALL_TEMP_FOLDERS:
+        	logger.info("Remove temporary folder: %s", temp_folder)
+        	shutil.rmtree(temp_folder)
+
 		# subprocess.call(['git', 'init'])
 		# subprocess.call(['git', 'add', '*'])
 		# subprocess.call(['git', 'commit', '-m', 'Initial commit'])
@@ -30,4 +40,5 @@ def post_hook():
     sys.exit(0)
 
 if __name__ == "__main__":
+    remove_temp_folders(ALL_TEMP_FOLDERS)
     post_hook()
